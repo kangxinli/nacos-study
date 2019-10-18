1c96b299628f7b5ccc012a6c623ea3dfff3f059b
kangxinli <kangxinli0201@163.com>
2019-10-18 22:09:26 +0800

    nacos-spring-cloud

--- nacos-spring-boot-config-mysql/src/main/java/com/sample/nacos/spring/boot/SpringBootMySQLApplication.java
+++ nacos-spring-boot-config-mysql/src/main/java/com/sample/nacos/spring/boot/SpringBootMySQLApplication.java
@@ -4,6 +4,19 @@ import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Nacos 控制台添加配置：
+ * Data ID：
+ * 		datasource.properties
+ * Group：
+ * 		DEFAULT_GROUP
+ * 配置内容示例：
+ * 		spring.datasource.url=jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf8
+ * 		spring.datasource.username=root
+ * 		spring.datasource.password=root
+ * 		spring.datasource.driver-class-name=com.mysql.jdbc.Driver
+ *
+ */
 @SpringBootApplication
 @NacosPropertySource(dataId = "mysql.properties")
 public class SpringBootMySQLApplication {
